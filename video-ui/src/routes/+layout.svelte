06f94d034cc0776ec93e17d4fa70e9e07e1f6996
swagatobhaskar <bhaskarswagato@gmail.com>
2026-04-22 12:57:08 +0530

    WIP

--- video-ui/src/routes/+layout.svelte
+++ video-ui/src/routes/+layout.svelte
@@ -6,4 +6,10 @@
 </script>
 
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
+
+<header class="w-full min-h-16 md:h-20 border-b border-gray-300">
+	<div class="p-6">
+		<p class="font-light text-3xl text-violet-950">Cartoon Dikhao</p>
+	</div>
+</header>
 {@render children()}
