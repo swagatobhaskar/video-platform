367bae14c69ebdcf2e30c91e9dafe5e089a7e4ec
swagatobhaskar <bhaskarswagato@gmail.com>
2026-05-13 15:27:28 +0530

    ADD: reusable components structure

---
+++ video-ui/src/lib/components/ui/VideoCard.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+    import Crab from "$lib/assets/crab-cool.jpeg";
+</script>
+
+<div class="w-90 border border-gray-300 rounded-sm shadow-md overflow-hidden bg-white">
+    <!-- Video Thumbnail -->
+    <div class="h-52 w-full overflow-hidden">
+        <img 
+            src={Crab}
+            alt="thumbnail"
+            class="w-full h-full object-cover"
+        />
+    </div>
+
+    <!-- Content -->
+    <div class="p-4 flex flex-col gap-2">
+        <!-- Title -->
+        <h2 class="text-lg font-sans font-semibold leading-snug text-black ">
+            Himalayan Crab Building A Concrete House By The Mediterrainean
+        </h2>
+
+        <!-- Category -->
+        <p class="text-sm text-gray-600 font-medium">
+            Animals
+        </p>
+
+        <!-- Meta -->
+        <div class="flex justify-between text-sm text-gray-500 pt-2">
+            <span>13 hours ago</span>
+            <span>1,46,000 views</span>
+        </div>
+        
+    </div>
+</div>
