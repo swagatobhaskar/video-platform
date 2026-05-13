c1cfcfe09ed0f6d4746eb3d11cd7ec824a0885cb
swagatobhaskar <bhaskarswagato@gmail.com>
2026-05-13 15:30:06 +0530

    WIP: home page

--- video-ui/src/routes/+page.svelte
+++ video-ui/src/routes/+page.svelte
@@ -1,1 +1,35 @@
-<h1 class="font-extrabold text-6xl font-serif text-gray-400">This is going to be the home page of this SvelteKit App.</h1>
+<script lang="ts">
+    import VideoCard from "$lib/components/ui/VideoCard.svelte";
+</script>
+
+<!-- Categories Buttons -->
+<section id="category-buttons" class="w-full h-16 py-4 flex flex-row items-center justify-evenly bg-mauve-500 text-white/60 font-medium">
+    <p class="py-2 px-3 bg-gray-400/40 rounded-sm">Hindi</p>
+    <p class="py-2 px-3 bg-gray-400/40 rounded-sm">Bengali</p>
+    <p class="py-2 px-3 bg-gray-400/40 rounded-sm">English</p>
+</section>
+
+<section id="video-list" class="p-12 grid grid-cols-5 gap-8">
+    <VideoCard />
+    <VideoCard />
+    <VideoCard />
+    <VideoCard />
+    <VideoCard />
+    <VideoCard />
+    <VideoCard />
+    <VideoCard />
+    <VideoCard />
+    <VideoCard />
+    <VideoCard />
+    <VideoCard />
+    <VideoCard />
+    <VideoCard />
+    <VideoCard />
+    <VideoCard />
+    <VideoCard />
+    <VideoCard />
+    <VideoCard />
+    <VideoCard />
+    <VideoCard />
+    <VideoCard />
+</section>
