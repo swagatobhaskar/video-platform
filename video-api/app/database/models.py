2456a36eabca62d6f19cc84d3fe87e1cf00fcbc3
swagatobhaskar <bhaskarswagato@gmail.com>
2026-06-14 15:22:03 +0530

    WIP: model definition

--- video-api/app/database/models.py
+++ video-api/app/database/models.py
@@ -1,8 +1,10 @@
+from typing import List
 import uuid
 from datetime import datetime
+import enum
 
-from sqlalchemy import String, DateTime, func
-from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy import String, DateTime, func, Text, Enum
+from sqlalchemy.dialects.postgresql import UUID, JSONB
 from sqlalchemy.orm import Mapped, mapped_column
 
 from app.database.session import Base
@@ -42,4 +44,49 @@ class User(Base):
     
     def __repr__(self) -> str:
         return f"<User(id={self.id}, email='{self.email}')>"
+    
+    
+class Category(Base):
+    __tablename__ = "categories"
+
+    id: Mapped[uuid.UUID] = mapped_column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+
+    name: Mapped[str] = mapped_column(String(255), unique=True, index=True, nullable=False)
+
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True),
+        server_default=func.now(),
+        nullable=False,
+    )
+
+    updated_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True),
+        server_default=func.now(),
+        onupdate=func.now(),
+        nullable=False,
+    )
+    
+    image_url: Mapped[str] = mapped_column(String(255), nullable=True)
+    
+    def __repr__(self) -> str:
+        return f"<Category(id={self.id}, name='{self.name}')>"
+    
+
+class LanguageEnum(enum.Enum):
+    HINDI = "hindi"
+    BENGALI = "bengali"
+
+class Video(Base):
+    __tablename__ = "videos"
+    
+    id: Mapped[uuid.UUID] = mapped_column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    
+    title: Mapped[str] = mapped_column(String(255), unique=True, index=True, nullable=False)
+    
+    description: Mapped[str] = mapped_column(Text, nullable=True)
+    
+    language: Mapped[LanguageEnum] = mapped_column(Enum(LanguageEnum), nullable=False, default=LanguageEnum.BENGALI)
+    
+    seo_tags: Mapped[List[str]] = mapped_column(JSONB, nullable=True, default=list)
+    
     
