f39463fdda4fbbe3d3abd9b875d4cbf12de89de6
Swagato Bhaskar <bhaskarswagato@gmail.com>
2026-07-05 23:14:47 +0530

    WIP: Video drop modal

--- video-ui/src/routes/upload/+page.svelte
+++ video-ui/src/routes/upload/+page.svelte
@@ -1,7 +1,22 @@
 <script lang="ts">
+    import Modal from "./_components/Modal.svelte";
+    import { onMount } from 'svelte';
 
+    let open = $state(false);
+
+    onMount(() => {
+        open = true;
+    })
 </script>
 
-<div class="">
-    
-</div>
+<!-- <buttton onclick={() => (open = true)}>
+    Open Modal
+</buttton> -->
+
+<Modal bind:open>
+    <h2>Hello!</h2>
+    <p>This is a modal.</p>
+    <button onclick={() => (open = false)}>
+        Close
+    </button>
+</Modal>
