d7cd97c33005125c794a586bb7b9fda78b8ad6a8
Swagato Bhaskar <bhaskarswagato@gmail.com>
2026-08-05 01:34:04 +0530

    WIP: Video-history route

--- video-ui/src/routes/upload-history/+page.svelte
+++ video-ui/src/routes/upload-history/+page.svelte
@@ -1,32 +1,32 @@
 <script lang="ts">
 
-    // let { data } = $props();
-    // const videos = $derived(data.uploads);
-
     import { onMount } from "svelte";
+    import type { VideoList } from "$lib/types/VideoList"
+    import VideoPublishProgressItem from "./VideoPublishProgressItem.svelte";
 
-    // const filters = ['draft', 'published', 'archived'];
+    const tabs = ["all", "draft", "published", "archived"];
+    let activeTab = $state(tabs[0]);    // Default: Draft
 
-    type Video = {
-        id: string;
-        title: string;
-        object_key: string;
-        publication_status: string;
-        created_at: Date;
-    }
-
-    let videos = $state<Video[]>([]);
+    let allVideos = $state<VideoList[]>([]);
+    
     let loading = $state(false);
 	let error = $state<string | null>(null);
 
+    const filteredVideos = $derived.by(() => {
+        if (activeTab === "all") return allVideos;
+
+        return allVideos.filter(
+            video => video.publication_status === activeTab
+        );
+    });
+
+    const handleTab = (tab: string) => {
+        activeTab = tab;
+    };
+
     const fetchAllVideos = async () => {
         try {
-            const response = await fetch('http://127.0.0.1:8000/api/video', {
-                method: 'GET',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-            });
+            const response = await fetch('http://127.0.0.1:8000/api/video');
 
             if (!response.ok) {
 				throw new Error(
@@ -34,7 +34,7 @@
 				);
 			}
 
-            videos = await response.json();
+            allVideos = await response.json();
             // console.log(videos);
         } catch(err) {
             console.error(err);
@@ -47,17 +47,37 @@
     onMount(fetchAllVideos);
 </script>
 
-<main class="">
-    {#if loading}
-        <p>Loading videos...</p>
-    {:else if error}
-        <p>{error}</p>
-    {:else}
-        {#each videos as video (video.id)}
-            <p>{video.id}</p>
-            <p>{video.title}</p>
-            <p>{video.publication_status}</p>
-            <p>{video.created_at}</p>
-        {/each}
-    {/if}    
+<main class="w-2/4 mx-auto">
+    <div class="flex justify-center py-6">
+        <div class="flex w-full max-w-4xl border-x border-gray-400">
+            {#each tabs as tab (tab)}
+                <button
+                    onclick={() => handleTab(tab)} //activeTab = tab}
+                    class="relative flex-1 py-3 text-lg transition-colors
+                           border-r last:border-r-0 border-gray-400 cursor-pointer"
+                >
+                    {tab}
+
+                    {#if activeTab === tab}
+                        <span
+                            class="absolute bottom-0 left-0 h-0.5 w-full bg-black"
+                        ></span>
+                    {/if}
+                </button>
+            {/each} 
+        </div>
+    </div>
+    <section class="p-4 w-full h-[calc(100%-5rem)] overflow-y-auto">
+        {#if loading}
+            <p>Loading videos...</p>
+        {:else if error}
+            <p>{error}</p>
+        {:else if filteredVideos.length === 0}
+            <p class="items-center text-center font-semibold text-3xl text-gray-400">No video</p>
+        {:else}
+            {#each filteredVideos as video (video.id)}
+                <VideoPublishProgressItem {video} />
+            {/each}
+        {/if}
+    </section>
 </main>
