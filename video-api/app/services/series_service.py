6b93bb762d38f7dacb8dc15a3721dc05f0ec40d9
Swagato Bhaskar <bhaskarswagato@gmail.com>
2026-08-15 23:05:05 +0530

    WIP: series services

--- video-api/app/services/series_service.py
+++ video-api/app/services/series_service.py
@@ -1,8 +1,40 @@
+import uuid
+
+from sqlalchemy.exc import SQLAlchemyError, IntegrityError
+from app.core.database import AsyncSession
+from app.models import Series
 from app.repositories.series_repository import SeriesRepository
 from app.repositories.video_repository import VideoRepository
+from app.exceptions.series import SeriesNotFound, SeriesAlreadyExists, VideoAlreadyInTheSeries
 
 class SeriesService:
-    def __init__(self, series_repo: SeriesRepository):
+    def __init__(self, series_repo: SeriesRepository, session: AsyncSession):
         self.series_repo = series_repo
+        self.session = session
 
-    async def 
+    async def list(self):
+        return await self.series_repo.list()
+
+
+    async def get_series_detail(self, id: uuid.UUID):
+        series = await self.series_repo.get_with_videos(id)
+
+        if not series:
+            raise SeriesNotFound()
+
+        return series
+
+
+    async def create(self, name: str) -> Series:
+        series = await self.series_repo.create(name)
+        try:
+            await self.session.commit()
+        except IntegrityError:
+            await self.session.rollback()
+            raise SeriesAlreadyExists()
+        except SQLAlchemyError:
+            await self.session.rollback()
+            raise
+        return series
+
+    
